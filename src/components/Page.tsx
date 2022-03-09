69cb5e6c23d67d40cc05b41f067925bae3c13fb6
Robin Lake <robinhlake@gmail.com>
2022-03-09 08:34:58 -0700

    scss and component folder

---
+++ src/components/Page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from './Form'
+
+function Page() {
+    return (
+        <div>
+            <h1>My Kinks</h1>
+            <Form />            
+        </div>
+    )
+}
+
+export default Page
